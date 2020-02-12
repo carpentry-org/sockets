edbb459fb909c51be907abca753a3ae8ea80f242
hellerve <veit@veitheller.de>
2020-02-12 15:29:54 +0100

    make Socket.read memsafe; fix Socket.setup-client-for

--- src/sockets.h
+++ src/sockets.h
@@ -1,7 +1,9 @@
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int Socket_buf_MINUS_size = 1024;
@@ -32,19 +34,24 @@ Socket Socket_setup_MINUS_client_MINUS_for(String* addr, String* proto, int port
   Socket ret;
   struct addrinfo* result;
   struct addrinfo* rp;
+  struct addrinfo hints;
 
-  ret.them.sin_family = AF_INET;
-  ret.them.sin_port = htons(port);
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = PF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
 
-  ret.valid = (getaddrinfo(addr, proto, NULL, &res)) == 0;
-  if (!ret.valid) return res;
+  ret.valid = getaddrinfo(*addr, *proto, &hints, &result) == 0;
+  if (!ret.valid) return ret;
 
   for (rp = result; rp != NULL; rp = rp->ai_next) {
     ret.socket = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 
-    ret.valid = connect(ret.socket, (struct sockaddr*)&(ret.them), sizeof(ret.them)) == 0;
+    ret.valid = connect(ret.socket, rp->ai_addr, rp->ai_addrlen) == 0;
     if (ret.valid) break;
+    close(ret.socket);
   }
+
+  freeaddrinfo(result);
 
   return ret;
 }
@@ -60,6 +67,10 @@ void Socket_send_MINUS_bytes(Socket* sock, Array* msg) {
 String Socket_read(Socket* sock) {
   String buf = CARP_MALLOC(Socket_buf_MINUS_size+1);
   int r = read(sock->socket, buf, Socket_buf_MINUS_size);
+  if (r == -1) {
+    buf[0] = '\0';
+    return buf;
+  }
   buf[r] = '\0';
   return buf;
 }
